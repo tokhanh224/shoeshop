e7fb07fe1b0c3b59b74aefb0b4e21460b17e8cf1
tokhanh224 <tokhanh224@gmail.com>
2024-11-11 10:16:29 +0700

    update css AuthForm

--- src/Admin/Auth/SignIn.tsx
+++ src/Admin/Auth/SignIn.tsx
@@ -50,50 +50,50 @@ const SignIn = () => {
   };
 
   return (
-    <div>
-      {contextHolder}
-      <div>
-        <Link to={`/`}>
-          <Button>
-            <BackwardFilled /> Back
-          </Button>
-        </Link>
+    <div className="signup-container">
+      <Link to={`/`}>
+        <Button type="primary" className="back-button">
+          <BackwardFilled /> Back
+        </Button>
+      </Link>
+      <div className="signup-page">
+        {contextHolder}
+        <h2>Sign in</h2>
+        <Form
+          name="basic"
+          labelCol={{ span: 8 }}
+          wrapperCol={{ span: 16 }}
+          style={{ maxWidth: 600 }}
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+          autoComplete="off"
+        >
+          <Form.Item<FieldType>
+            label="email"
+            name="email"
+            rules={[
+              { required: true, type: "email", message: "Please input email!" },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item<FieldType>
+            label="password"
+            name="password"
+            rules={[{ required: true, message: "Please input password!" }]}
+          >
+            <Input.Password />
+          </Form.Item>
+          <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+            <Button type="primary" htmlType="submit">
+              SignIn
+            </Button>
+            <p>Dont have account? <Link to={`/signup`}>
+            <ins>SignUp here.</ins>
+            </Link></p>
+          </Form.Item>
+        </Form>
       </div>
-      <Form
-        name="basic"
-        labelCol={{ span: 8 }}
-        wrapperCol={{ span: 16 }}
-        style={{ maxWidth: 600 }}
-        initialValues={{ remember: true }}
-        onFinish={onFinish}
-        autoComplete="off"
-      >
-        <Form.Item<FieldType>
-          label="Email"
-          name="email"
-          rules={[
-            {
-              required: true,
-              type: "email",
-              message: "Please input your email!",
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item<FieldType>
-          label="Password"
-          name="password"
-          rules={[{ required: true, message: "Please input your password!" }]}
-        >
-          <Input.Password />
-        </Form.Item>
-        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-          <Button type="primary" htmlType="submit">
-            Sign In
-          </Button>
-        </Form.Item>
-      </Form>
     </div>
   );
 };
--- src/Admin/Auth/SignUp.tsx
+++ src/Admin/Auth/SignUp.tsx
@@ -5,12 +5,13 @@ import type { FormProps } from "antd";
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
 import React from "react";
+import "./Auth.css";
 type FieldType = {
   email?: string;
   password?: string;
 };
 const SignUp = () => {
-  const nav = useNavigate()
+  const nav = useNavigate();
   const [messageAPI, contextHolder] = message.useMessage();
   const { mutate } = useMutation({
     mutationFn: async (users: FieldType) => {
@@ -18,56 +19,60 @@ const SignUp = () => {
         await axios.post(`http://localhost:5000/signup`, users);
         messageAPI.success("SignUp success");
       } catch (error) {
-        messageAPI.error((error as any).response.data)
+        messageAPI.error((error as any).response.data);
       }
     },
   });
   const onFinish: FormProps<FieldType>["onFinish"] = (values) => {
     console.log("Success:", values);
     mutate(values);
-    nav("/signin")
+    nav("/signin");
   };
   return (
-    <div>
-      {contextHolder}
-      <div>
-        <Link to={`/`}>
-        <Button>
-          <BackwardFilled/>Back
+    <div className="signup-container">
+      <Link to={`/`}>
+        <Button type="primary" className="back-button">
+          <BackwardFilled /> Back
         </Button>
-        </Link>
+      </Link>
+      <div className="signup-page">
+        {contextHolder}
+        <h2>Sign Up</h2>
+        <Form
+          name="basic"
+          labelCol={{ span: 8 }}
+          wrapperCol={{ span: 16 }}
+          style={{ maxWidth: 600 }}
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+          autoComplete="off"
+        >
+          <Form.Item<FieldType>
+            label="email"
+            name="email"
+            rules={[
+              { required: true, type: "email", message: "Please input email!" },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item<FieldType>
+            label="password"
+            name="password"
+            rules={[{ required: true, message: "Please input password!" }]}
+          >
+            <Input.Password />
+          </Form.Item>
+          <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+            <Button type="primary" htmlType="submit">
+              SignUp
+            </Button>
+             <Link to={`/signin`}>
+            <ins>SignIn</ins>
+            </Link>
+          </Form.Item>
+        </Form>
       </div>
-      <Form
-        name="basic"
-        labelCol={{ span: 8 }}
-        wrapperCol={{ span: 16 }}
-        style={{ maxWidth: 600 }}
-        initialValues={{ remember: true }}
-        onFinish={onFinish}
-        autoComplete="off"
-      >
-        <Form.Item<FieldType>
-          label="email"
-          name="email"
-          rules={[
-            { required: true, type: "email", message: "Please input email!" },
-          ]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item<FieldType>
-          label="password"
-          name="password"
-          rules={[{ required: true, message: "Please input password !" }]}
-        >
-          <Input.Password />
-        </Form.Item>
-        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-          <Button type="primary" htmlType="submit">
-            Submit
-          </Button>
-        </Form.Item>
-      </Form>
     </div>
   );
 };
